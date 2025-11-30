90163b2e9d250fd0bbb08eb58b910e938d0d514e
livanshmalhotra <livanshmalhotra05@gmail.com>
2025-11-30 17:43:18 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+const app = express()
+
+
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+
+app.get("/",(request,response)=>{
+    response.status(200).json({message:"success from backend "})
+})
+
+app.listen(ENV.PORT,()=>console.log("server is running on port:",ENV.PORT))
