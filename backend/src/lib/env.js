9d3edb647c8a210f0e4817313397b2ddff08c62f
livanshmalhotra <livanshmalhotra05@gmail.com>
2025-12-14 02:20:15 +0530

    auth and db sync

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -9,6 +9,7 @@ export const ENV={
     NODE_ENV:process.env.NODE_ENV,
     STREAM_API_KEY:process.env.STREAM_API_KEY,
     STREAM_API_SECRET:process.env.STREAM_API_SECRET,
-    CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY
+    CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
+    CLIENT_URL:process.env.CLIENT_URL
 };
 
